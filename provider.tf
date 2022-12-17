e3d99531a67f4d9ef70e4643866186ea0e731b8c
Mahmut Tasdemir <mahmut.tasdemir@deop.ca>
2022-12-17 11:54:31 -0500

    asd

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.36.0"
+    }
+  }
+}
+backend "remote" {
+    organization = "mahmuttasdemir"
+ 
+    workspaces {
+      name = "SaturdayErolterraform"
+    }
+  }
+provider "azurerm" {
+  # Configuration options
+}
